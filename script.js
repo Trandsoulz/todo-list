cb3874049837963d947f36dbe5cd9617a3277b30
Bethrand <bethrand2019@gmail.com>
2022-06-15 18:39:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log(`Hello World!`);
+
+// const first = document.querySelector('#first-element');
+// const firstDiv = document.createElement('div');
+// firstDiv.innerHTML = 'Hello World!'
+// const h11 = first.append(firstDiv);
+
+// console.log(h11)
+
+
