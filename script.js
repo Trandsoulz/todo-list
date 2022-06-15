82469795dbf5eb739b5ec7b6c3279934d07a2d8d
Bethrand <bethrand2019@gmail.com>
2022-06-15 23:32:05 +0100

    Interactive adding and removing of box

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ console.log(`Hello World!`);
 // console.log(h11)
 
 
+const add = document.querySelector('#icon2');
+const remove = document.querySelector('.icon3');
+const form = document.querySelector('.middle');
+const body = document.body;
+
+add.addEventListener('click', () => {
+    form.classList.toggle('addlist');
+    form.classList.remove('removelist');
+    add.classList.toggle('none');
+    remove.classList.toggle('block');
+});
+
+remove.addEventListener('click', () => {
+    form.classList.toggle('removelist');
+    form.classList.remove('addlist');
+    remove.classList.toggle('block');
+    add.classList.toggle('none');
+
+});
